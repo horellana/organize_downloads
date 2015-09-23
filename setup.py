69a37ac25e12fecc469833236bb0debbabc972bc
Hector Orellana <pulga.p.29@gmail.com>
2015-09-22 22:17:11 -0300

    setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='orgnaize_downloads.py',
+      version='0.1.5',
+      description='A program to keep my download folder organized',
+      url='https://github.com/juiko/organize_downloads',
+      license='GPL3',
+      author='Hector Orellana',
+      author_email='hofm92@gmail.com',
+      scripts=['organize_downloads.py'],
+      )
